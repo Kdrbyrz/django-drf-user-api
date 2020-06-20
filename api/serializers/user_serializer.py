4c695703f7ecc8e34b391dac929b6aaf1628dc64
kadir <kadirboyraz78@gmail.com>
2020-06-20 23:56:37 +0300

    User Serializer Created

---
+++ api/serializers/user_serializer.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from api.models.user import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'email', 'first_name', 'last_name', 'bio',]
