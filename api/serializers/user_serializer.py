43d172c46ce7c720492c9e6163ca5fb293098773
kadir <kadirboyraz78@gmail.com>
2020-06-21 20:28:09 +0300

    Typo Fix

--- api/serializers/user_serializer.py
+++ api/serializers/user_serializer.py
@@ -5,4 +5,4 @@ from api.models.user import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['username', 'password', 'email', 'first_name', 'last_name', 'bio',]
+        fields = ["username", "password", "email", "first_name", "last_name", "bio"]
